1312a8e8e2debb82e4abe84c4375d4d5869d14c7
VitaliiTymko <intehmet2015@gmail.com>
2022-10-14 19:58:33 +0300

    hw-7 finish

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,22 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
 const galleryContainer = document.querySelector(".gallery");
-// console.log(galleryItemsNew);
-const imageMarkup = galleryItemsMarkup (galleryItems);
+const imageMarkup = galleryItemsMarkup(galleryItems);
 
-// galleryContainer.insertAdjacentHTML('beforeend', imageMarkup);
+galleryContainer.insertAdjacentHTML("beforeend", imageMarkup);
 
-galleryContainer.addEventListener('click', onGalleryContainerClick);
+galleryContainer.addEventListener("click", onGalleryContainerClick);
 
-function galleryItemsMarkup (galleryItems) {
- return galleryItems.map(({ preview, original, description }) => {
-    return
-    `<div class="gallery__item">
-    <a class="gallery__link" href="${original}">
-      <img
+function galleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+       <div class="gallery__item">
+       <a class="gallery__link" href="${original}">
+       <img
         class="gallery__image"
         src="${preview}"
         data-source="${original}"
@@ -25,29 +25,33 @@ function galleryItemsMarkup (galleryItems) {
     </a>
    </div>
     `;
- })
- .join("");
-}
-
-function onGalleryContainerClick (event) {
-    blockStandartAction(event);
-    if (event.target.nodeName !== "IMG") {
-        return;
-    }
-    // console.log(event.target.dataset.source);
-    const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`)
-
-instance.show();
-
-galleryContainer.addEventListener("keydown", (event) => {
-    if(event.code === "Escape") {
-        instance.close();
-    }
-})
+    })
+    .join("");
 }
 
 function blockStandartAction(event) {
-    event.preventDefault();
+  event.preventDefault();
 }
+
+function onGalleryContainerClick(event) {
+  blockStandartAction(event);
+// event.preventDefault();
+console.log(event.target.nodeName);
+  if (event.target.nodeName !== "IMG") {
+    
+    return;
+  }
+  console.log(event.target.dataset.source);
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+
+  instance.show();
+
+  galleryContainer.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+}
+
